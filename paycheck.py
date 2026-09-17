750af757ef3000376125471b575427323b506f2d
Adam Williams <thisguyadam14@gmail.com>
2026-09-17 11:17:55 -0400

    Add author name and date to paycheck.py
    Added name and date to the top of the paycheck.py file.
    added inputs

--- paycheck.py
+++ paycheck.py
@@ -1,5 +1,5 @@
-# Name:
-# Date:
+# Name: Adam Williams
+# Date: 09/18/2026
 # Course: COMP 163
 # Project 1: Paycheck Calculator
 
@@ -10,7 +10,11 @@
 #   2. Hours worked
 #   3. Hourly rate
 #   4. Tax rate, as a percent (for example, 10 means 10%)
-#
+employee = input()
+hours_worked = float(input())
+hourly_rate = float(input())
+tax_rate = float(input())
+
 # Hours worked and the hourly rate can have a fraction in them, like 37.5
 # hours or 10.25 hours. Use float() for all three numbers, not int().
 # int("37.5") crashes.
